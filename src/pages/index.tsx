1993b9503f4dda985b56b51105c69a3f4ed043d5
Sean Tolbert <sean@Seans-MacBook-Pro.local>
2023-05-05 17:08:01 -0400

    rendering posts from prisma studio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,11 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -26,7 +28,13 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        {/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
+
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}> {post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
